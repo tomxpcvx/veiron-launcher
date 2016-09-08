9a3587dcb9dfc4cdb2de12dbcbce7979b64910e3
tomxpcvx <dev@tomxpcvx.wtf>
2016-09-08 20:14:40 +0200

    Added mockup design for the launcher

--- VeironLauncher/src/de/veironlauncher/VeironLauncher.java
+++ VeironLauncher/src/de/veironlauncher/VeironLauncher.java
@@ -5,6 +5,8 @@
  */
 package de.veironlauncher;
 
+import javax.swing.UIManager;
+
 /**
  *
  * @author tompi
@@ -15,7 +17,13 @@ public class VeironLauncher {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(Exception e) {
+            
+        }
+        new Launcher().setVisible(true);
     }
     
 }
